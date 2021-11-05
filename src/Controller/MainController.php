bcdd767e2d9095597dbe8ddae0db952b87230106
TonyNavas <tonynavas777@gmail.com>
2021-11-04 22:02:07 -0600

    Cree una funcion para crear una vista xd

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,19 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\Crud;
+use App\Form\CrudType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
     /**
-     * @Route("/main", name="main")
+     * @Route("/", name="main")
      */
     public function index(): Response
     {
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
+        ]);
+    }
+    /**
+     * @Route("create", name="create")
+     */
+    public function create(Request $request){
+        $crud = new Crud();
+        $form = $this->createForm(CrudType::class, $crud);
+        $form->handleRequest($request);
+
+        return $this->render('main/create.html.twig',[
+            'form' => $form->createView()
         ]);
     }
 }
